e7a841903cf6e871883d71266355d1e02e0695ca
Micah Shute <micah.shute@gmail.com>
2018-02-15 16:26:04 -0500

    Removed integral as property from ATM, added to equation itself

--- winnerPayout.js
+++ winnerPayout.js
@@ -59,12 +59,11 @@ function EarningsEquation(coefficient, order){
 //using the EarningEquation constructor above. 
 var winningsATM = {
 	earningsEquation: new EarningsEquation(1,3),
-	integralEquation: (new EarningsEquation(1,3)).integralEquation(),
 	payUsers: function(winnersArray, maxPay = 100, minPay = 1){
 		var partitionsSize = 1 / winnersArray.length;
 		var weightCoefficient = 1;
 		for(var i = 0; i < winnersArray.length; i++){
-			var areaUnderCurve = this.integralEquation.y(1.0 - (partitionsSize * i)) - this.integralEquation.y(1.0 - (partitionsSize * (i + 1)));
+			var areaUnderCurve = this.earningsEquation.integralEquation().y(1.0 - (partitionsSize * i)) - this.earningsEquation.integralEquation().y(1.0 - (partitionsSize * (i + 1)));
 			if(i == 0){
 				weightCoefficient = maxPay / areaUnderCurve;
 			}
@@ -81,14 +80,16 @@ var winningsATM = {
 	}
 }
 
-
-//Using the above functions and objects to perform a sucessful payout
+//*************************************************************************************
+//**************************** Code Demonstration *************************************
+// Using the above functions and objects to perform a sucessful payout
+//Default pay equation -> y = 1*(x^3)
 
 winningsATM.payUsers(users);
 
-//test the payment
+//Test the payment
 for(var i = 0; i < users.length; i++){
-	console.log(users[i].coinTotal);
+	console.log(`${users[i].userName} payment: ${users[i].coinTotal}`);
 }
 console.log("Test complete");
 
@@ -105,16 +106,13 @@ winningsATM.payUsers(users, 70, 5);
 //test the payment
 
 for(var i = 0; i < users.length; i++){
-	console.log(users[i].coinTotal);
+	console.log(`${users[i].userName} payment: ${users[i].coinTotal}`);
 }
 console.log("Test complete");
+
+
 //Change the equation from 1*(x^3) to 3*(x^2)
-//NOTE: may be better to add a getter/setter for earningsEquation and integralEquation in order to 
-//resetboth with the same command. Will look into it/
-
-
 winningsATM.earningsEquation = new EarningsEquation(3,2);
-winningsATM.integralEquation = winningsATM.earningsEquation.integralEquation();
 
 //reset users array
 
@@ -129,7 +127,7 @@ winningsATM.payUsers(users);
 
 //test payment
 for(var i = 0; i < users.length; i++){
-	console.log(users[i].coinTotal);
+	console.log(`${users[i].userName} payment: ${users[i].coinTotal}`);
 }
 console.log("Test complete");
 
