851c5fe104f83adadeb69df32f6f298ee5d94b4e
Micah Shute <micah.shute@gmail.com>
2018-02-15 14:38:44 -0500

    Creating algorithm for user payout
    Creating algorithm for user payout

---
+++ winnerPayout.js
@@ -0,0 +1,97 @@
+
+//User prototype for testing of this code
+var users = [];
+
+function User(userName, coinTotal){
+	this.userName = userName;
+	this.coinTotal = coinTotal;
+}
+
+
+for(var i = 0; i < 10; i++){
+	users.push(new User(`User ${i + 1}`, 0))
+}
+
+
+//easier to write than to figure out how to import Math library
+function power(x,y){
+	if(y<0){
+		return 1/(power(x,(-1 * y)));
+	}
+
+	var product = 1;
+	for(var i = 0; i < y; i++){
+		product *= x;
+	}
+	return product
+}
+
+//easier to write than figure out how to import Math library
+function roundToInt(x){
+	var fraction = x - parseInt(x);
+	var firstDecimalPlace = parseInt(fraction * 10);
+	if(firstDecimalPlace >= 5){
+		return parseInt(x) + 1;
+	}else{
+		return parseInt(x);
+	}
+}
+
+
+
+//Constructor for desired equation. This way we can change the equation
+//we want to use by constructing a new EarningsEquation Object if the first one doesn't 
+//work well
+function EarningsEquation(coefficient, order){
+	this.coefficient = coefficient;
+	this.order = order;
+	this.y = function(x){
+		return coefficient * power(x,order);
+	};
+	this.integralEquation = function(){
+		return (new EarningsEquation(this.coefficient / (order + 1), order + 1));
+	}
+
+}
+
+// function IntegralEquation(equation){
+// 	return EarningsEquation(equation.coefficient / (equation.order + 1), equation.order + 1)
+// }
+
+//Object used to pay users. Equation used to calculate payment can be set 
+//using the EarningEquation constructor above. 
+var winningsATM = {
+	earningsEquation: new EarningsEquation(1,3),
+	integralEquation: (new EarningsEquation(1,3)).integralEquation(),
+	payUsers: function(winnersArray, maxPay = 100, minPay = 1){
+		var partitionsSize = 1 / winnersArray.length;
+		var weightCoefficient = 1;
+		for(var i = 0; i < winnersArray.length; i++){
+			var areaUnderCurve = this.integralEquation.y(1.0 - (partitionsSize * i)) - this.integralEquation.y(1.0 - (partitionsSize * (i + 1)));
+			if(i == 0){
+				weightCoefficient = maxPay / areaUnderCurve;
+			}
+			var payout = areaUnderCurve * weightCoefficient;
+			var intPayout = roundToInt(payout);
+			console.log(intPayout);
+			if(intPayout < minPay){
+				intPayout = minPay;
+			}
+			this.conductPayout(winnersArray[i], intPayout);
+		}
+	},
+	conductPayout: function(user, amount){
+		user.cointTotal = user.cointTotal + amount;
+	}
+}
+
+
+//Using the above functions and objects to perform a sucessful payout
+
+winningsATM.payUsers(users);
+
+//test the payment
+
+for(var i = 0; i < users.length; i++){
+	console.log(users[i].coinTotal);
+}
